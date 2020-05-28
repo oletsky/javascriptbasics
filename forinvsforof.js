9a3e4745c7f0422f8fa7d0dcccb51f3eef10113a
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-28 14:47:42 +0300

    Add files via upload

---
+++ forinvsforof.js
@@ -0,0 +1,14 @@
+//The difference between for-in and for-of loops in JavaScript
+
+mas=[10, 20, 30];
+
+console.log("In:")
+for (const c in mas) {
+    console.log(c); //0 1 2
+}
+
+console.log("--------------");
+console.log("Of:")
+for (const c of mas) {
+    console.log(c); //10 20 30
+}
