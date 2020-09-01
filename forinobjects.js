4b54576a2f67c6574d0aa219e40cd7866cfd8429
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-01 12:59:09 +0300

    Add files via upload

---
+++ forinobjects.js
@@ -0,0 +1,4 @@
+let obj = {x: 5, y: 10};
+for (let prop in obj) {
+    console.log(prop+" - "+obj[prop]);
+}
---
+++ foroftrap.js
@@ -0,0 +1,6 @@
+let m=[10,20,30];
+console.log(m);
+for (let c of m) {
+    c=0;
+}
+console.log(m);
